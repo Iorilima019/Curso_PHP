acde942cd9d137f0a531338789f23750adffe70f
Iori <iorilima13@gmail.com>
2026-04-22 21:56:59 -0300

    Create 06_Array.php

---
+++ 06_Array.php
@@ -0,0 +1,22 @@
+<?php
+/*
+$carros = ['Uno', 'Gol', 'Fusion', 'Civic', 'BYD'];
+echo $carros[0];
+echo '<br>';
+echo $carros[1];
+echo '<br>';
+echo $carros[2];
+echo '<br>';
+echo $carros[3];
+echo '<br>';
+echo $carros[4];
+*/
+
+$carroUm = [
+     'Uno',
+     'Fiat',
+     'Branco'];
+
+
+echo $carroUm[2];
+?>
