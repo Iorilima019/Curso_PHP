6f964af134aa340ec7a7e70fe3b372276b0c8029
Iori <iorilima13@gmail.com>
2026-04-22 21:56:47 -0300

    Create 04_Variaveis.php

---
+++ 04_Variaveis.php
@@ -0,0 +1,29 @@
+<?php
+$name = 'Carlos Ferreira';
+echo $name;
+echo '<br>';
+
+$name = 'João Carlos';
+echo $name;
+echo '<br>';
+
+$name = 12.2;
+echo $name;
+echo '<br>';
+
+$name = 123;
+echo $name;
+echo '<br>';
+
+$name = 'System Of A Dowm';
+echo $name;
+echo '<br>';
+
+$name = 'Sanza';
+echo $name;
+echo '<br>';
+
+$name = 'Gorillaz';
+echo $name;
+echo '<br>';
+?>
