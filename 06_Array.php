8d4a45f0a4612fb69160ca99b8beb9deff772461
Iori <iorilima13@gmail.com>
2026-04-27 20:14:26 -0300

    Update 06_Array.php
    trabalhando com Arrays

--- 06_Array.php
+++ 06_Array.php
@@ -22,4 +22,56 @@ echo '<br>';
 echo $carroUm['modelo'];
 echo '<br>';
 echo $carroUm['marca'];
+ echo '<br>';
+echo'<hr>';
+
+$carroDois=[
+'modelo' => 'Dolphin',
+    'marca' => 'BYD',
+    'cor' => 'Rosa Pessego'];
+
+echo $carroDois['cor'];
+echo '<br>';
+echo $carroDois['modelo'];
+echo '<br>';
+echo $carroDois['marca'];
+ echo '<br>';
+echo'<hr>';
+
+$carroTres=[
+'modelo' => 'Corolla GR',
+    'marca' => 'Toyota',
+    'cor' => 'Preto'];
+
+echo $carroTres['cor'];
+echo '<br>';
+echo $carroTres['modelo'];
+echo '<br>';
+echo $carroTres['marca'];
+ echo '<br>';
+echo'<hr>';
+
+$carroQuatro=[
+'modelo' => 'Lancer Evolutin',
+    'marca' => 'Mitsubishi',
+    'cor' => 'Escarlate'];
+
+echo $carroQuatro['cor'];
+echo '<br>';
+echo $carroQuatro['modelo'];
+echo '<br>';
+echo $carroQuatro['marca'];
+ echo '<br>';
+ echo'<hr>';
+
+$carroCin=[
+'modelo' => 'Lancer GT',
+    'marca' => 'Mitsubishi',
+    'cor' => 'Violeta'];
+
+echo $carroCin['cor'];
+echo '<br>';
+echo $carroCin['modelo'];
+echo '<br>';
+echo $carroCin['marca'];
 ?>
