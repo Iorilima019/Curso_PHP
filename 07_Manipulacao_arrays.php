abb173ff454ea6d476d2ae79ad4614c4790f8bc8
Iori <iorilima13@gmail.com>
2026-04-27 21:31:56 -0300

    Create 07_Manipulacao_arrays.php

---
+++ 07_Manipulacao_arrays.php
@@ -0,0 +1,23 @@
+<?php
+$infoEmprese = [
+    'nome' => 'Senac Americana',
+    'funcionarios' => 'Paulo Santos',
+    'anoAtual' => 2026
+];
+
+$inFoCursos =[
+    'cursos' => ['php', 'JS', 'Vue JS', 'Laravel'],
+    'totlCursos' => 26,
+];
+echo'<pre>';
+//Arreys Keys serve para mosts as chaves do Array
+//var_dump(array_keys($infoEmpresa));
+
+//var_dump(array_values($infoEmpresa));
+
+//var_dump(count($infoEmpresa));
+
+//$infoEmpresa = array_merge($infoEmpresa, $infpCursos);
+//print_r($infoEmpresa);
+
+?>
