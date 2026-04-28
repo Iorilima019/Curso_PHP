b8868791cb843b9916d01084b26e31357247c181
Iori <iorilima13@gmail.com>
2026-04-27 22:06:38 -0300

    Update 08_Manipulacao_arrays.php

--- 08_Manipulacao_arrays.php
+++ 08_Manipulacao_arrays.php
@@ -1,5 +1,42 @@
 <?php
-$carrinho = ['arroz' , 'Sabão', 'Feijão', 'Balinhas'];
+$carrinho = ['arroz' ,
+'<br>',
+ 'Sabão', 
+'<br>',
+ 'Feijão',
+'<br>',
+  'Balinhas'];
 var_dump($carrinho);
 
+echo '<hr>';
+
+array_pop($carrinho);
+var_dump($carrinho);
+
+echo'<hr>';
+
+array_shift($carrinho);
+var_dump($carrinho);
+
+echo'<hr>';
+//remove da memoria um mopvimento do array
+unset($carrinho[0]);
+var_dump($carrinho);
+
+echo '<hr>';
+//adiciona elementos no array
+array_push($carrinho, 'Tapete');
+array_push($carrinho, 'Rodo');
+var_dump($carrinho);
+
+echo'<hr>';
+//add em primeiro lugar
+array_unshift($carrinho, 'Microondas');
+array_unshift($carrinho, 'Tapete');
+var_dump($carrinho);
+
+echo'<hr>';
+
+$carrinho = array_unique($carrinho);
+var_dump($carrinho);
 ?>
